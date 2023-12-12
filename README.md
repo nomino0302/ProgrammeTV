7c48dda26c4993fd38eaad39d5cddc1bce4101f4
nomino0302 <0302nomino@gmail.com>
2023-12-12 19:38:23 +0100

    Ajout des résumés et du logging

--- README.md
+++ README.md
@@ -32,3 +32,42 @@ NumFree = https://www.programme-tv.net/programme/free-13/
 NumBouygues = https://www.programme-tv.net/programme/bouygues-24/
 NumCanal = https://www.programme-tv.net/programme/canal-5/
 ```
+
+**La BDD est insensible à la casse**
+
+## Table 'Chaines'
+| Colonne               | Type                  | Description                            |
+|-----------------------|-----------------------|----------------------------------------|
+| `idChaine`            | INT AUTO_INCREMENT    | Clé primaire, identifiant unique       |
+| `nomChaine`           | TEXT                  | Nom de la chaîne                       |
+| `numTNT`              | INT                   | Numéro de la chaîne sur TNT            |
+| `numOrange`           | INT                   | Numéro de la chaîne chez Orange        |
+| `numSFR`              | INT                   | Numéro de la chaîne chez SFR           |
+| `numFree`             | INT                   | Numéro de la chaîne chez Free          |
+| `numBouygues`         | INT                   | Numéro de la chaîne chez Bouygues      |
+| `numCanal`            | INT                   | Numéro de la chaîne chez Canal         |
+| `urlChaine`           | TEXT                  | URL de la chaîne                       |
+| `urlLogo`             | TEXT                  | URL du logo de la chaîne               |
+
+## Table 'Programmation'
+| Colonne               | Type                  | Description                            |
+|-----------------------|-----------------------|----------------------------------------|
+| `idProgrammation`     | INT AUTO_INCREMENT    | Clé primaire, identifiant unique       |
+| `idChaine`            | INT                   | Clé étrangère vers la table Chaines    |
+| `dateEmission`        | DATE                  | Date de l'émission                     |
+| `heureEmission`       | TIME                  | Heure de l'émission                    |
+| `nomEmission`         | TEXT                  | Nom de l'émission                      |
+| `nomEpisode`          | TEXT                  | Nom de l'épisode                       |
+| `typeEpisode`         | TEXT                  | Type de l'épisode                      |
+| `dureeEpisode`        | TEXT                  | Durée de l'épisode                     |
+| `urlEpisode`          | TEXT                  | URL de l'épisode                       |
+| `urlVignette`         | TEXT                  | URL de la vignette de l'épisode        |
+| `urlProgrammeChaineDate` | TEXT              | URL du programme de la chaîne pour une date donnée |
+
+## Table 'Resumes'
+| Colonne               | Type                  | Description                            |
+|-----------------------|-----------------------|----------------------------------------|
+| `idResume`            | INT AUTO_INCREMENT    | Clé primaire, identifiant unique       |
+| `idProgrammation`     | INT                   | Clé étrangère vers la table Programmation |
+| `nomCompletEpisode`   | TEXT                  | Nom complet de l'épisode              |
+| `resumeEpisode`       | TEXT                  | Résumé de l'épisode                    |
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import mysql.connector
-from mysql.connector.cursor import MySQLCursor
 import configparser
 import requests
 from bs4 import BeautifulSoup
 import traceback
 from datetime import datetime, timedelta
+import sys
+import os
+import logging
+import pickle
 
 
-def init_database(cursor: MySQLCursor):
+def init_database():
     """Permet de créer la database et les tables si elles n'existent pas"""
+
+    # Reset de la base de donnée si RESET = True
+    if RESET:
+        cursor.execute("DROP DATABASE IF EXISTS ProgrammeTV;")
+        logger.debug("Base de donnée 'ProgrammeTV' supprimée")
+
 
     # Créer la base de données (insensible à la casse) si elle n'existe pas
     cursor.execute("CREATE DATABASE IF NOT EXISTS ProgrammeTV CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
@@ -45,24 +54,47 @@ def init_database(cursor: MySQLCursor):
             urlVignette TEXT,
             urlProgrammeChaineDate TEXT,
             PRIMARY KEY (idProgrammation),
-            FOREIGN KEY (idChaine) REFERENCES Chaines(idChaine)
+            FOREIGN KEY (idChaine) REFERENCES Chaines(idChaine) ON DELETE CASCADE
+        );
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Resumes (
+            idResume INT AUTO_INCREMENT,
+            idProgrammation INT,
+            nomCompletEpisode TEXT,
+            resumeEpisode TEXT,
+            PRIMARY KEY (idResume),
+            FOREIGN KEY (idProgrammation) REFERENCES Programmation(idProgrammation) ON DELETE CASCADE
         );
     """)
 
     cursor.execute("USE ProgrammeTV;")
 
+    logger.debug("Bases de données et tables correctement créées et utilisées")
 
-def get_channels(cursor: MySQLCursor, session: requests.Session, fournisseurs: dict):
+
+def update_channels():
     """Permet de mettre à jour les chaînes dans la base de donnée"""
 
+    # On récupère les noms de chaines déjà présentes dans la BDD
     cursor.execute("SELECT nomChaine FROM Chaines;")
     database_chaines = [item[0].lower() for item in cursor.fetchall()]
 
     for column_name, url_fournisseur in fournisseurs.items():
 
-        page = session.get(url_fournisseur)
+        # On récupère les données de l'URL (le code HTML) avec un timeout de 5 secondes
+        try:
+            page = session.get(url_fournisseur, timeout=5)
+            if not page.ok:
+                raise RuntimeError(f"Code {page.status_code}")
+        except Exception as err:
+            logger.warning(f"Erreur {url_fournisseur} : {err}")
+            continue
+            
         soup = BeautifulSoup(page.content, "html.parser")
 
+        # On récupère les numéros de chaines, les nom de chaines et les URLs de chaines et de logo, avec les valeurs nettoyées dans des listes
         numeros = [int(num.get_text().strip().replace("N°", "")) if num else None for num in soup.find_all(class_="gridRow-cardsChannelNumber")]
         chaines = [chaine.get_text().strip() if chaine else None for chaine in soup.find_all(class_="gridRow-cardsChannelItemLink")]
         chaines = [chaines[i].replace(f"N°{numeros[i]}", "").strip() if chaines[i] else None for i in range(len(numeros))]
@@ -81,24 +113,29 @@ def get_channels(cursor: MySQLCursor, session: requests.Session, fournisseurs: d
                     imgs_urls.append(None)
             else:
                 imgs_urls.append(None)
+        
+        logger.debug(f"[{column_name}] numeros[len()={len(numeros)},None={numeros.count(None)}] / chaines[len()={len(chaines)},None={chaines.count(None)}] / urls[len()={len(urls)},None={urls.count(None)}] / imgs_urls[len()={len(imgs_urls)},None={imgs_urls.count(None)}]")
 
         for i in range(len(numeros)):
 
+            # On ajoute la chaine dans Chaines si elle n'existe pas, on update le numéro de chaine dans tous les cas
             if chaines[i].lower() not in database_chaines:
                 query = "INSERT INTO Chaines (nomChaine, urlChaine, urlLogo) VALUES (%s, %s, %s);"
                 values = (chaines[i], urls[i], imgs_urls[i])
                 cursor.execute(query, values)
                 database_chaines.append(chaines[i].lower())
+                logger.debug(f"[{column_name}] {chaines[i]} ajouté à la BDD")
             query = f"UPDATE Chaines SET {column_name} = %s WHERE nomChaine = %s;"
             values = (numeros[i], chaines[i])
             cursor.execute(query, values)
 
 
-def delete_old_shows(cursor: MySQLCursor):
+def delete_old_shows():
     """Supprime les données trop vieilles (avant hier) et renvoie la liste de dates à récupérer"""
 
     today = datetime.now().date()
 
+    # On récupère les dates utilisés dans Programmations et on fait plusieurs liste qu'on va traiter après
     cursor.execute("SELECT DISTINCT(dateEmission) FROM Programmation;")
     database_dates = [item[0] for item in cursor.fetchall()]
     to_have_dates = [today + timedelta(days=i) for i in range(-1, 8)]
@@ -113,17 +150,20 @@ def delete_old_shows(cursor: MySQLCursor):
         query = f"DELETE FROM Programmation WHERE dateEmission = %s;"
         values = (date,)
         cursor.execute(query, values)
+        logger.debug(f"Programmation et Resumes de {date} supprimés")
 
+    logger.debug(f"Dates à récupérer : {needed_dates}")
     return needed_dates
 
 
-def get_shows(cursor: MySQLCursor, needed_dates: set, session: requests.Session, nb_etapes: int):
+def get_shows(needed_dates: list, nb_etapes: int):
     """Permet de mettre à jour les programmes TV : on supprime les données des programmes d'avant hier,
     On réactualise les programmes d'aujourd'hui, et on récupére le programme de dans 7 jours"""
 
     cursor.execute("SELECT idChaine, urlChaine FROM Chaines;")
     ids_urls = cursor.fetchall()
 
+    # Opérations max à faire (boucles)
     nb_operations = len(ids_urls) * len(needed_dates)
     nb = 0
 
@@ -132,13 +172,24 @@ def get_shows(cursor: MySQLCursor, needed_dates: set, session: requests.Session,
 
             print(f"(4/{nb_etapes}) Récupération des émissions et mise à jour de la BDD ({nb}/{nb_operations}) ", end="\r")
 
+            # url = URL de base de la chaine, url_chaine = URL avec la date
             url_chaine = url.split("/")
             url_chaine.insert(-1, str(date))
             url_chaine = "/".join(url_chaine)
 
-            page = session.get(url)
+            logger.debug(f"[{id} | {date}] ({nb}/{nb_operations}) Récupération de {url_chaine}")
+
+            try:
+                page = session.get(url_chaine, timeout=5)
+                if not page.ok:
+                    raise RuntimeError(f"Code {page.status_code}")
+            except Exception as err:
+                logger.warning(f"Erreur {url_chaine} : {err}")
+                continue
+
             soup = BeautifulSoup(page.content, "html.parser")
 
+            # Meme principe que pour update_channels
             heures = [datetime.strptime(heure.get_text().strip(), "%Hh%M").time() if heure else None for heure in soup.find_all(class_="mainBroadcastCard-startingHour")]
             emissions_elts = [emission_elt if emission_elt else None for emission_elt in soup.find_all(class_="mainBroadcastCard-title")]
             emissions = [emission.get_text().strip() if emission else None for emission in emissions_elts]
@@ -174,41 +225,157 @@ def get_shows(cursor: MySQLCursor, needed_dates: set, session: requests.Session,
                         vignettes_urls.append(None)
                 else:
                     vignettes_urls.append(None)
+            
+            logger.debug(f"[{id} | {date}] ({nb}/{nb_operations}) heures[len()={len(heures)},None={heures.count(None)}] / emissions[len()={len(emissions)},None={emissions.count(None)}] / episodes[len()={len(episodes)},None={episodes.count(None)}] / types_episodes[len()={len(types_episodes)},None={types_episodes.count(None)}] / durees[len()={len(durees)},None={durees.count(None)}] / urls[len()={len(urls)},None={urls.count(None)}] / vignettes_urls[len()={len(vignettes_urls)},None={vignettes_urls.count(None)}]")
 
+            # On ajoute dans la BDD dans tous les cas
             for i in range(len(heures)):
                 query = "INSERT INTO Programmation (idChaine, dateEmission, heureEmission, nomEmission, nomEpisode, typeEpisode, dureeEpisode, urlEpisode, urlVignette, urlProgrammeChaineDate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
                 values = (id, date, heures[i], emissions[i], episodes[i], types_episodes[i], durees[i], urls[i], vignettes_urls[i], url_chaine)
                 cursor.execute(query, values)
-            
+            logger.debug(f"[{id} | {date}] ({nb}/{nb_operations}) Données enregistrées dans la BDD")
+
             nb += 1
     
     print(f"(4/{nb_etapes}) Récupération des émissions et mise à jour de la BDD ({nb}/{nb_operations}) ", end="")
 
 
-def main(cursor: MySQLCursor, session: requests.Session, fournisseurs: dict):
+def get_resumes(needed_dates: list, nb_etapes: int):
+    """Permet de récupérer les résumés des épisodes et de mettre à jour la base de donnée"""
+
+    formatted_dates = ', '.join(f"'{date}'" for date in needed_dates)
+    cursor.execute(f"SELECT idProgrammation, urlEpisode FROM Programmation WHERE dateEmission IN ({formatted_dates});")
+    ids_urls = cursor.fetchall()
+
+    nb_operations = len(ids_urls)
+    nb = 0
+
+    for id, url in ids_urls:
+
+        print(f"(5/{nb_etapes}) Récupération des résumées et mise à jour de la BDD ({nb}/{nb_operations}) ", end="\r")
+        logger.debug(f"[{id}] ({nb}/{nb_operations}) Récupération de {url}")
+
+        try:
+            page = session.get(url, timeout=5)
+            if not page.ok:
+                raise RuntimeError(f"Code {page.status_code}")
+        except Exception as err:
+            logger.warning(f"Erreur {url} : {err}")
+            continue
+
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        # Toutes les émissions/épisodes n'ont pas de "résumé", seulement ceux qui en ont seront enregistrés dans la BDD
+        if soup.find(class_="programCollectionEpisode-synopsis"):
+            vrai_titre = soup.find(class_="synopsis-title")
+            if vrai_titre:
+                vrai_titre = vrai_titre.get_text().strip().replace("Résumé ", "")
+            
+            resume = soup.find(class_="synopsis-teaser")
+            if resume:
+                resume = resume.get_text().strip()
+            
+            logger.debug(f"[{id}] ({nb}/{nb_operations}) vrai_titre[None={vrai_titre is None}] / resume[None={resume is None}]")
+            
+            query = "INSERT INTO Resumes (idProgrammation, nomCompletEpisode, resumeEpisode) VALUES (%s, %s, %s);"
+            values = (id, vrai_titre, resume)
+            cursor.execute(query, values)
+            logger.debug(f"[{id}] ({nb}/{nb_operations}) Données enregistrées dans la BDD")
+
+        nb += 1
     
-    nb_etapes = 4
+    print(f"(5/{nb_etapes}) Récupération des résumées et mise à jour de la BDD ({nb}/{nb_operations}) ", end="")
 
-    print(f"(1/{nb_etapes}) Préparation de la base de donnée ", end="")
-    init_database(cursor)
+
+def clear_channels():
+    """Fonction qui permet de supprimer les chaines dont 'idChaine' ne sont pas présentent en tant que clés étrangères dans Programmation"""
+
+    cursor.execute("SELECT idChaine, nomChaine FROM Chaines WHERE idChaine NOT IN (SELECT DISTINCT idChaine FROM Programmation);")
+    to_delete_ids_chaines = cursor.fetchall()
+    for id, chaine in to_delete_ids_chaines:
+        query = "DELETE FROM Chaines WHERE idChaine = %s;"
+        values = (id,)
+        cursor.execute(query, values)
+        logger.debug(f"Chaine {chaine} (idChaine={id}) supprimée")
+
+
+def main():
+    """Fonction principale, elle exécute toutes les autres fonctions"""
+    
+    nb_etapes = 6
+
+    # On utilise "START TRANSACTION ... COMMIT" à toutes les fonctions au cas où elles
+    # viendraient à retourner une exception, elles retrouveront leurs états d'origine (ROLLBACK dans le else à la fin du script)
+    cursor.execute("START TRANSACTION;")
+    print(f"(1/{nb_etapes}) Préparation de la base de donnée (RESET={RESET}) ", end="")
+    init_database()
     print("..... OK!")
+    cursor.execute("COMMIT;")
 
+    cursor.execute("START TRANSACTION;")
     print(f"(2/{nb_etapes}) Récupération des chaînes et mise à jour de la BDD ", end="")
-    get_channels(cursor, session, fournisseurs)
+    update_channels()
     print("..... OK!")
+    cursor.execute("COMMIT;")
 
-    print(f"(3/{nb_etapes}) Suppression des anciennes données ", end="")
-    needed_dates = delete_old_shows(cursor)
+    cursor.execute("START TRANSACTION;")
+    print(f"(3/{nb_etapes}) Suppression des anciennes données (non comprises entre J-1 et J+7) ", end="")
+    needed_dates = delete_old_shows()
     print("..... OK!")
+    cursor.execute("COMMIT;")
 
+    cursor.execute("START TRANSACTION;")
     print(f"(4/{nb_etapes}) Récupération des émissions et mise à jour de la BDD ", end="\r")
-    get_shows(cursor, needed_dates, session, nb_etapes)
+    get_shows(needed_dates, nb_etapes)
     print("..... OK!")
+    cursor.execute("COMMIT;")
+
+    cursor.execute("START TRANSACTION;")
+    print(f"(5/{nb_etapes}) Récupération des résumées et mise à jour de la BDD ", end="\r")
+    get_resumes(needed_dates, nb_etapes)
+    print("..... OK!")
+    cursor.execute("COMMIT;")
+
+    cursor.execute("START TRANSACTION;")
+    print(f"(6/{nb_etapes}) Suppression des chaînes non utilisées (notamment par Programmation)", end="")
+    clear_channels()
+    print("..... OK!")
+    cursor.execute("COMMIT;")
+
+    print("Base de donnée ProgrammeTV à jour !")
+    logger.info("Base de donnée ProgrammeTV à jour !")
 
 
 if __name__ == "__main__":
 
-    DEBUG = True
+    DEBUG = False
+    RESET = False
+
+    if "-d" in sys.argv or "--debug" in sys.argv:
+        DEBUG = True
+    if "-r" in sys.argv or "--reset" in sys.argv:
+        RESET = True
+
+    # Créer le dossier 'logs' si nécessaire
+    log_dir = 'logs'
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+    
+    # Suppression des fichiers log si + de 5
+    for file in os.listdir(log_dir):
+        if file[-4:] != ".log":
+            os.remove(os.path.join(log_dir, file))
+    liste_logs = sorted(os.listdir(log_dir))
+    for i in range(len(liste_logs) - 4): # 4 anciens + 1 nouveau
+        os.remove(os.path.join(log_dir, liste_logs[i]))
+
+    # Configuration du logger
+    logging.basicConfig(level=logging.DEBUG, filename=os.path.join(log_dir, f"{datetime.now()}.log"),
+                        format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s')
+    logging.getLogger("requests").setLevel(logging.WARNING) # logging souhaité du module 'requests'
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)  # Ou le niveau de logging souhaité
 
      # Lire le fichier de configuration
     config = configparser.ConfigParser()
@@ -223,20 +390,32 @@ if __name__ == "__main__":
         password=cred['password'],
     )
     cursor = conn.cursor()
+    logger.info(f"Connecté à MariaDB {cred['host']} avec l'utilisateur {cred['user']}")
 
+    # On récupère le fichier cookies.pkl si il existe
     session = requests.Session()
+    if "cookies.pkl" in os.listdir():
+        with open("cookies.pkl", "rb") as f:
+            session.cookies.update(pickle.load(f))
+        logger.info("Fichier cookies.pkl chargé")
 
     try:
-        cursor.execute("START TRANSACTION;")
-        main(cursor, session, fournisseurs)
+        main()
     except Exception as err:
+        print()
         if DEBUG:
             traceback.print_exc()
         else:
             print(f"Erreur : {err}")
+        logger.error(f"Erreur : {err}", exc_info=True)
+        # Si il y a une erreur, on ne prend pas en compte les nouvelles requêtes SQL
         cursor.execute("ROLLBACK;")
-    else:
-         cursor.execute("COMMIT;")
     finally:
         cursor.close()
         conn.close()
+        logger.info(f"Déconnecté de MariaDB {cred['host']}")
+
+        # Sauvegarde des cookies
+        with open("cookies.pkl", "wb") as f:
+            pickle.dump(session.cookies, f)
+        logger.info("Fichier cookies.pkl sauvegardé")
